2358bbfac8b883d16f9b267f2c528dd5bd1d1ee0
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-03 18:34:54 +0600

    Module #1. Task #1. Build Tools. Gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,19 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        boolean isAllPositive = false;
+        for (String e : args) {
+            if (!StringUtils.isPositiveNumber(e)) {
+                return isAllPositive;
+            }
+        }
+        isAllPositive = true;
+        return isAllPositive;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,15 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        //here magic will happen
+        int strInt = NumberUtils.toInt(str);
+
+        if (strInt > 0) {
+            return true;
+        } else return false;
     }
 }
