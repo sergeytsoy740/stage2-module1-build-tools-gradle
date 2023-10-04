665cc8408bd4b01f125bc277a3fb7ff4377c4e1e
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-04 18:46:55 +0600

    Module #1. Task #1. Build Tools. Gradle (7 - isCreat)

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -6,21 +6,22 @@ import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
 //    public static void main(String[] args) {
 //        System.out.println(isPositiveNumber("088"));
-//        System.out.println(NumberUtils.toDouble("088"));
+//        System.out.println(NumberUtils.isDigits("088"));
+//        System.out.println(NumberUtils.isCreatable("088"));
 //    }
 
     public static boolean isPositiveNumber(String str) {
 
-        if (str == null) {
-            return false;
-        }
+//        if (str == null) {
+//            return false;
+//        }
+//
+//        String[] chars = str.split("");
+//        if (chars[0].equals("0")) {
+//            return false;
+//        }
 
-        String[] chars = str.split("");
-        if (chars[0].equals("0")) {
-            return false;
-        }
-
-        if (NumberUtils.toDouble(str) > 0) {
+        if (NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0) {
             return true;
         } else return false;
 
