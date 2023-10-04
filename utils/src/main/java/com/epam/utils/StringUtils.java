63b0a7d1c47ba25fb9db8c06bbcbab680e889d6a
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-04 16:54:26 +0600

    Module #1. Task #1. Build Tools. Gradle (4 - double)

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,13 +2,12 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+
 public class StringUtils {
 
     public static boolean isPositiveNumber(String str) {
         //here magic will happen
-        int strInt = NumberUtils.toInt(str);
-
-        if (strInt > 0) {
+        if (NumberUtils.toDouble(str) > 0) {
             return true;
         } else return false;
     }
